c5844888ecd228c90fec6f8c85595f42bc0327f5
Nicola Reyes <nicolascribbles@gmail.com>
2021-06-26 01:48:42 -0600

    Creating the infrastructure for API calls

--- src/components/App/home.tsx
+++ src/components/App/home.tsx
@@ -1,7 +1,19 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 
 const Home: React.FC = () =>{
   const [name, setName] = useState("Nicola");
-  return ( <h4 className="pt-3">Hello, {name}</h4>
+  
+  useEffect(() => {
+    fetch("https://www.dnd5eapi.co/api/spells/acid-arrow/")
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+    });
+  }, [])
+  
+  return ( 
+    <div>
+      <h4 className="pt-3">Hello, {name}</h4>
+    </div>
 )}
 export default Home;
